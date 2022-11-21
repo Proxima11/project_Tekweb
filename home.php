ccad613e3876d57c508ce074f51133da4498eede
unknown <c14210275@john.petra.ac.id>
2022-11-21 12:06:57 +0700

    Home
    Bikin home
    Search & library nyusul

---
+++ home.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="bootstrap-5.2.0/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="home.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+	<style>
+		body{font: 14px sans-serif;}
+		.wrapper{width: 1500px; padding: 20px; margin: 0 auto;}
+	</style>
+</head>
+<body>
+	<div class="wrapper">
+		<div class="row">
+			<div class="col-4">
+				<h2>Tekweb Home Music</h2>
+				<nav class="nav flex-column">
+  					<a class="nav-link active" aria-current="page" href="home.php"><i class="fa-light fa-house"></i>Home</a>
+  					<a class="nav-link" href="#"><i class="fa-regular fa-magnifying-glass"></i>Search</a>
+  					<a class="nav-link" href="#"><i class="fa-light fa-books"></i>Library</a>
+				</nav>
+			</div>
+			<div class="col-8">
+				<div id="account">...</div>
+				<div id="recom1">
+					<h3>Recommendations</h3>
+					<div class="row">
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+				</div>
+				<br>
+				<div id="recom2">
+					<h3>HOT</h3>
+					<div class="row">
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					      </div>
+					    </div>
+					  </div>
+					  <div class="col">
+					    <div class="card h-100">
+					      <img src="..." class="card-img-top" alt="...">
+					      <div class="card-body">
+					        <h5 class="card-title">Card title</h5>
+					        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
