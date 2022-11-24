c5049d2f1ec58386fe3ca25d65e4735ed6724f74
Proxima11 <103930241+Proxima11@users.noreply.github.com>
2022-11-24 23:06:31 +0700

    benerin icon

--- homepage.php
+++ homepage.php
@@ -15,9 +15,9 @@
 			<div class="col-4">
 				<h2>Tekweb Home Music</h2>
 				<nav class="nav flex-column">
-  					<a class="nav-link active" aria-current="page" href="home.php"><i class="fa-light fa-house"></i>Home</a>
-  					<a class="nav-link" href="search.php"><i class="fa-regular fa-magnifying-glass"></i>Search</a>
-  					<a class="nav-link" href="library.php"><i class="fa-light fa-books"></i>Library</a>
+  					<a class="nav-link active" aria-current="page" href="homepage.php">Home  <i class="fa fa-home"></i></a>
+  					<a class="nav-link" href="search.php">Search  <i class="fa fa-search"></i></a>
+  					<a class="nav-link" href="library.php">Library  <i class="fa fa-book"></i></a>
 				</nav>
 			</div>
 			<div class="col-8">
