cd0cc160a734a3afcd9e73db320d67119edef25d
Proxima11 <103930241+Proxima11@users.noreply.github.com>
2022-11-28 20:25:33 +0700

    animasi note

--- reset-code.php
+++ reset-code.php
@@ -12,10 +12,67 @@ if($email == false){
     <title>Code Verification</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="login-usercss.css">
+    <style>
+        .float {
+            -webkit-animation-name: Floating;
+            -webkit-animation-duration: 6s;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-timing-function: ease-in-out;
+        }
+
+        @-webkit-keyframes Floating{
+            from {-webkit-transform:translate(0, 0px);}
+            65% {-webkit-transform:translate(0, 50px);}
+            to {-webkit-transform: translate(0, -0px);    }    
+        }
+
+        .spin1 {
+            -webkit-animation-name: Spinning;
+            -webkit-animation-duration: 12s;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-timing-function: ease-in-out;
+        }
+
+        .spin2 {
+            -webkit-animation-name: Spinning;
+            -webkit-animation-duration: 25s;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-timing-function: ease-in-out;
+        }
+
+        .spin3 {
+            -webkit-animation-name: Spinning;
+            -webkit-animation-duration: 16s;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-timing-function: ease-in-out;
+        }
+
+        .spin4 {
+            -webkit-animation-name: Spinning;
+            -webkit-animation-duration: 19s;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-timing-function: ease-in-out;
+        }
+
+        @-webkit-keyframes Spinning{
+            0% {
+            -webkit-transform: rotate(0deg);
+        }
+            100% {
+                -webkit-transform: rotate(360deg);
+            } 
+        }
+    </style>
 </head>
 <body>
     <div class="container">
         <div class="row">
+            <div class="float col-md-4 offset-md-4" style="min-width: 0px; max-height: 0px;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="spin1" style="fill: white; width: 30px; height: 30px; position: relative; left: 425px;"><path d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="spin2" style="fill: white; width: 60px; height: 60px; position: relative; right: 150px;"><path d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="spin3" style="fill: white; width: 60px; height: 60px; position: relative; left: 275px; bottom: -200px;"><path d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="spin4" style="fill: white; width: 30px; height: 30px; position: relative; right: 325px; bottom: -150px;"><path d="M499.1 6.3c8.1 6 12.9 15.6 12.9 25.7v72V368c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V147L192 223.8V432c0 44.2-43 80-96 80s-96-35.8-96-80s43-80 96-80c11.2 0 22 1.6 32 4.6V200 128c0-14.1 9.3-26.6 22.8-30.7l320-96c9.7-2.9 20.2-1.1 28.3 5z"/></svg>
+        </div>
             <div class="col-md-4 offset-md-4 form">
                 <form action="reset-code.php" method="POST" autocomplete="off">
                     <h2 class="text-center">Code Verification</h2>
--- testsidebar1.php
+++ testsidebar1.php
@@ -5,12 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <!----======== CSS ======== -->
     <link rel="stylesheet" href="testsidebar1css.css">
-    
-    <!----===== Boxicons CSS ===== -->
-    <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+
     
     <!--<title>Dashboard Sidebar Menu</title>--> 
 </head>
@@ -28,14 +26,14 @@
                 </div>
             </div>
 
-            <i class='bx bx-chevron-right toggle'></i>
+            <i class='bx bx-chevron-right toggle'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></i>
         </header>
 
         <div class="menu-bar">
             <div class="menu">
 
                 <li class="search-box">
-                    <i class='bx bx-search icon'></i>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352c79.5 0 144-64.5 144-144s-64.5-144-144-144S64 128.5 64 208s64.5 144 144 144z"/></svg>
                     <input type="text" placeholder="Search...">
                 </li>
 
